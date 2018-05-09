bee78a2fa2224c67d03e170e51978165aa567211
Sydney Barlow <sydneybarlow44@gmail.com>
2018-05-09 04:42:25 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -28,7 +28,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array,element) {
 }
 
 function removeElementFromBeginningOfArray(array,element) {
-  array.slice(1)
+  return array.slice(1)
 }
 
 function destructivelyRemoveElementFromEndOfArray (array,element) {
