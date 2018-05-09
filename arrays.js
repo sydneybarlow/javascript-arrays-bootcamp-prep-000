8fd9a81ce8aa102abeec1dfe7f934d879f2844b6
Sydney Barlow <sydneybarlow44@gmail.com>
2018-05-09 04:05:42 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,43 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(array, element) {
+  return [element, ...array]
+}
+
+function destructivelyAddElementToBeginningOfArray(array,element) {
+   array.unshift(element)
+   return array
+}
+
+function addElementToEndOfArray(array,element) {
+  return [...array, element]
+}
+
+function destructivelyAddElementToEndOfArray(array,element) {
+  array.push(element)
+  return array
+}
+
+function accessElementInArray(array,element) {
+  return array[element]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array,element) {
+  array.shift(element)
+  return array
+}
+
+function removeElementFromBeginningOfArray(array,element) {
+  array.slice(0)
+  return array
+}
+
+function destructivelyRemoveElementFromEndOfArray (array,element) {
+  array.pop()
+  return array
+}
+
+function removeElementFromEndOfArray (array,element) {
+  array.slice(0, array.length - element)
+}
+
